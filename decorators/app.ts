d2bbdd9e7d9d7bb54cba008ea421912cbd524734
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2020-06-29 15:47:45 -0500

    adding log method decorator and new people route

--- decorators/app.ts
+++ decorators/app.ts
@@ -5,10 +5,28 @@ const server = new APIServer();
 
 class APIRoutes {
 
+    @logRoute()
     @route("get", "/")
     public indexRoute(req: Request, res: Response) {
         return {
             "Hello": "World"
+        }
+    }
+
+    @logRoute()
+    @route("get", "/people")
+    public peopleRoute(req: Request, res: Response) {
+        return {
+            people: [
+                {
+                    "firstName": "Dharmesh",
+                    "lastName": "Kamdar"
+                },
+                {
+                    "firstName": "Mark",
+                    "lastName": "Brown"
+                },
+            ]
         }
     }
 }
@@ -18,6 +36,17 @@ function route(method: string, path: string): MethodDecorator {
         server.app[method](path, (req: Request, res:Response) => {
             res.status(200).json(descriptor.value(req, res));
         })
+    }
+}
+
+function logRoute(): MethodDecorator {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const original = descriptor.value;
+        descriptor.value = function (...args: any[]) {
+            let req = args[0] as Request;
+            console.log(`${req.url} ${req.method} Called`);
+            return original.apply(this, args);
+        }
     }
 }
 
