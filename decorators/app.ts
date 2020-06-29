ab6acba599ef6267c07a903f42da09a07b2384ce
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2020-06-29 15:40:28 -0500

    adding API Server and route with method decorator

---
+++ decorators/app.ts
@@ -0,0 +1,24 @@
+import APIServer from "./APIServer";
+import { Request, Response} from "express";
+
+const server = new APIServer();
+
+class APIRoutes {
+
+    @route("get", "/")
+    public indexRoute(req: Request, res: Response) {
+        return {
+            "Hello": "World"
+        }
+    }
+}
+
+function route(method: string, path: string): MethodDecorator {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        server.app[method](path, (req: Request, res:Response) => {
+            res.status(200).json(descriptor.value(req, res));
+        })
+    }
+}
+
+server.start();
